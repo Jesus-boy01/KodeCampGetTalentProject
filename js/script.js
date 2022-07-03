a2eaa43d4684fe190deb1c06eab6856f2a1f59f4
Tolulope Dada <tdada0912@gmail.com>
2022-07-03 16:41:38 +0100

    more on corrections and added a functionality to the button when the input fields are focused on

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const password = document.getElementById("password");
 const togglePassword = document.getElementById("toggle-password");
+const email = document.getElementById("email");
+const signIn = document.getElementById("signin");
 
 togglePassword.addEventListener('click', function () {
     const type = password.getAttribute("type") === "password" ? "text" : "password";
@@ -9,9 +11,16 @@ togglePassword.addEventListener('click', function () {
 });
 
 password.addEventListener('focus', iconDisplay);
+password.addEventListener('focus', changeButtonColor);
 
 function iconDisplay() {
     if (!(onfocus)) {
         togglePassword.classList.add("icon-display");
     }
+}
+
+email.addEventListener('focus', changeButtonColor);
+
+function changeButtonColor() {
+    signIn.classList.add("button-active-color")
 }
