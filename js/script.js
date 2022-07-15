4ad454d52932ebdd575247f4187f729cfe48476c
Tolulope Dada <tdada0912@gmail.com>
2022-07-15 11:51:55 +0100

    code optimization

--- js/script.js
+++ js/script.js
@@ -37,39 +37,39 @@ function signinSuccess(e) {
 }
 
 function formValidation() {
-    if (email.value && password.value) {
-        // modalFunction();
-    } else {
+    if (!(email.value && password.value)) {
         alert("Email and Password must be filled");
     }
 }
 
 function addModalFunction() {
-    successModal.innerHTML = `<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-xl modal-dialog-centered">
-                                <div class="modal-content border-0">
-                                    <div class="modal-header px-3 pt-3 border-0" style="background-color: #3AB418; border-color: #3AB418 !important;">
-                                        <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-title sign-up-success-head text-center" style="background-color: #3AB418; border-color: #3AB418 !important;">
-                                        <div class="sign-up-title text-white pb-3">
-                                            <h3 class="display-6">Awesome!</h3>
+    if (email.value && password.value) {
+        successModal.innerHTML = `<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-xl modal-dialog-centered">
+                                    <div class="modal-content border-0">
+                                        <div class="modal-header px-3 pt-3 border-0" style="background-color: #3AB418; border-color: #3AB418 !important;">
+                                            <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
-                                    </div>
-                                    <div class="modal-body">
-                                        <div class="sign-up-success-content text-center">
-                                            <div class="sign-up-image mx-auto">
-                                                <img src="img/sign-up-success.png" class="img-fluid" alt="Sign Up Success">
-                                            </div>
-                                            <div class="sign-up-success-info pb-lg-3 pt-lg-2">
-                                                <p class="mx-2" style="color: #555;">Your account has been successfully created!</p>
-                                            </div>
-                                            <div class="sign-up-success-button mb-lg-5 mb-md-4 mb-4">
-                                                <a href="home.html" class="text-decoration-none text-center fs-lg-5 btn btn-primary py-2 mx-auto" style="width: 30%; background-color: #2776EA; border-color: #2776EA;">Get Started</a>
+                                        <div class="modal-title sign-up-success-head text-center" style="background-color: #3AB418; border-color: #3AB418 !important;">
+                                            <div class="sign-up-title text-white pb-3">
+                                                <h3 class="display-6">Awesome!</h3>
                                             </div>
                                         </div>
+                                        <div class="modal-body">
+                                            <div class="sign-up-success-content text-center">
+                                                <div class="sign-up-image mx-auto">
+                                                    <img src="img/sign-up-success.png" class="img-fluid" alt="Sign Up Success">
+                                                </div>
+                                                <div class="sign-up-success-info pb-lg-3 pt-lg-2">
+                                                    <p class="mx-2" style="color: #555;">Your account has been successfully created!</p>
+                                                </div>
+                                                <div class="sign-up-success-button mb-lg-5 mb-md-4 mb-4">
+                                                    <a href="home.html" class="text-decoration-none text-center fs-lg-5 btn btn-primary py-2 mx-auto" style="width: 30%; background-color: #2776EA; border-color: #2776EA;">Get Started</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                </div>
-                            </div>`;
+                                    </div>
+                                </div>`;
+    }
 }
